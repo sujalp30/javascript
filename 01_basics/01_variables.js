49b3a7a7e036be8959f94866e95eab33877ea1c9
Sujal Patel <sujalpatel752@gmail.com>
2025-03-03 17:43:02 +0000

    variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 123456
+let accountEmail = "sujal@gmail.com"
+var accountPassword = "654321"
+accountCity = "Vadodara"
+let accountState;
+
+
+accountEmail = "hc@gmail.com"
+accountPassword = "21212121"
+accountCity = "Indore"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail , accountPassword , accountCity , accountState])
