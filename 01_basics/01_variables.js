48e9c70b59cafdd467affcc4f884e1d401c4be79
Sujal Patel <sujalpatel752@gmail.com>
2025-03-04 18:17:55 +0000

    data types

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -11,4 +11,8 @@ accountCity = "Indore"
 
 console.log(accountId);
 
+
+// prefer not to use var 
+// because of issue in block scope and functional scope 
+
 console.table([accountId, accountEmail , accountPassword , accountCity , accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict";      // treat all JS code as newer version
+
+// alert(3 + 3)    // we are using node js , not browser......this will run in browser engine
+
+
+console.log(3 + 3) ; console.log("Sujal")   // code should be in readeable form 
+
+console.log(3 + 3) 
+console.log("Sujal")                        // code should be in readable form like this.
+
+let name = "Sujal"
+let age = 18
+let isLoggedin = false
+let state;
+
+//data types:-
+
+// number => 2 to the power 53
+// BigInt
+
+// String => ""
+// Boolean => true or false
+// null => standalone value.........it is not undefined it is empty.
+// undefined =>
+// Symbol => for uniqueness
+
+
+// Object
+
+console.log(typeof null);               // typeof shows , the type of what you want. for eg - typeof null 
+console.log(typeof undefined);
