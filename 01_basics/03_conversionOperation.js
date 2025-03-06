89ebc92d58cde410d57e3923a37d14c38d4417af
Sujal Patel <sujalpatel752@gmail.com>
2025-03-06 09:40:09 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,15 +1,15 @@
 let score = "33abc"
 //let scoree = 33
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 //console.log(typeof scoree);
 
 
 // conversion in number 
 let valueInNumber = Number(score)       // this will change the type of
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 /*
     NOTE:-
 
@@ -29,7 +29,7 @@ console.log(valueInNumber);
 let isLoggedIn = "Sujal"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 /* 
     1 => true ; 0 => false
     "" => false
@@ -45,5 +45,52 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
+// ************************OPERATIONS************************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 ="hello"
+let str2 = " Sujal"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+
+
+// console.log(1 + 2);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log("1" + (2 + 2));
+// console.log(1 + 2 + "2");
+
+
+
+console.log(+true);
+//console.log(true+);    // this will show error
+console.log(+"");
+
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
