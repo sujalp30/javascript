02f9838ae55fd5927bff06e25d26cab1db16e86c
Sujal Patel <sujalpatel752@gmail.com>
2025-03-22 08:04:28 +0000

    date and time

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,22 @@
+// DATES    
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+
+
+
+
+
+let myCustomDate = new Date(2023 , 2 , 22)
+console.log(myCustomDate.toDateString());
+
+
+let myCustomDateAndTime = new Date(2023 , 2 , 22 , 5 , 3)
+console.log(myCustomDateAndTime.toLocaleString());
