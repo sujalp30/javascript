fcd25859446ea747b5eecb00f75c55c394032174
Sujal Patel <sujalpatel752@gmail.com>
2025-03-24 14:13:54 +0000

    scopess

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,7 +1,7 @@
 if (true) {
     let a = 10
     const b = 20   
-    console.log("Inner " , a);
+    // console.log("Inner " , a);
     
 }
 
@@ -9,4 +9,29 @@ if (true) {
 // for (let index = 0; index < array.length; index++) {
 //     const element = array[index]; 
 // }
+
+
+
+// NESTED SCOPES (FUNCTION KE ANDAR FUNCTION)
      
+function  one () {
+    const username = "Sujal"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);  
+    }
+    two()
+}
+one()
+
+
+
+if (true) {
+    const name = "Sujal"
+    if (name === "Sujal") {
+        const web = " Youtube"
+        console.log(name + web);
+        
+    }
+}
